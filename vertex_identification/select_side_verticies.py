3adbc3b60a36611566dd686b09debde489fe23a4
SRTJHSST <swesik@gmail.com>
2022-01-17 17:32:22 -0500

    working add and cancel buttons

--- vertex_identification/select_side_verticies.py
+++ vertex_identification/select_side_verticies.py
@@ -16,28 +16,46 @@ from PIL import ImageTk, Image
 # 	if event == cv2.EVENT_LBUTTONDOWN:
 # 	    side_features.append((x,y))
 
-def get_x_and_y(event):
-    global canvas, core_features
+# def get_x_and_y(event):
+#     global canvas, core_features
+#     lasx, lasy = event.x, event.y
+#     update_type = input("add, update, or cancel [a,u,c]? -----")
+#     if update_type == 'a':
+#         core_features.append((lasx,lasy))
+#         canvas.create_oval(lasx-5, lasy-5, lasx+5, lasy+5,fill="green", tags="circle"+str(len(core_features)))
+#         canvas.create_text(lasx, lasy, text=str(len(core_features)), fill="black", font=('Helvetica 15 bold'), tags="circle"+str(len(core_features)))
+#     elif update_type == 'u':
+#         to_update = input("which number do you want to update? ----- ")
+#         core_features[int(to_update)] = core_features.append((lasx,lasy))
+#         canvas.delete("circle"+to_update)
+#         canvas.create_oval(lasx-6, lasy-6, lasx+6, lasy+6,fill="green", tags="circle"+to_update)
+#         canvas.create_text(lasx, lasy, text=to_update, fill="black", font=('Helvetica 13 bold'), tags="circle"+to_update)
+#     elif update_type == 'c':
+#         pass
+#     else:
+#         print('error, wrong input')
+#         return
+
+def select_point(event):
+    global canvas, lasx, lasy
     lasx, lasy = event.x, event.y
-    update_type = input("add, update, or cancel [a,u,c]? -----")
-    if update_type == 'a':
-        core_features.append((lasx,lasy))
-        canvas.create_oval(lasx-5, lasy-5, lasx+5, lasy+5,fill="green", tags="circle"+str(len(core_features)))
-        canvas.create_text(lasx, lasy, text=str(len(core_features)), fill="black", font=('Helvetica 15 bold'), tags="circle"+str(len(core_features)))
-    elif update_type == 'u':
-        to_update = input("which number do you want to update? ----- ")
-        core_features[int(to_update)] = core_features.append((lasx,lasy))
-        canvas.delete("circle"+to_update)
-        canvas.create_oval(lasx-6, lasy-6, lasx+6, lasy+6,fill="green", tags="circle"+to_update)
-        canvas.create_text(lasx, lasy, text=to_update, fill="black", font=('Helvetica 13 bold'), tags="circle"+to_update)
-    elif update_type == 'c':
-        pass
-    else:
-        print('error, wrong input')
-        return
+    canvas.delete("lascircle")
+    canvas.create_oval(lasx-5, lasy-5, lasx+5, lasy+5,fill="cyan", tags="lascircle")
     
     
     print("(" + str(lasx) + "," + str(lasy) + ")")
+
+def add_vertex():
+    global canvas, core_features
+    canvas.delete("lascircle")
+    r = 6
+    canvas.create_oval(lasx-r, lasy-r, lasx+r, lasy+r,fill="green", tags="circle"+str(len(core_features)))
+    canvas.create_text(lasx, lasy, text=str(len(core_features)), fill="white", font=('Helvetica 12 bold'), tags="circle"+str(len(core_features)))
+    core_features.append((lasx,lasy))
+
+def cancel_vertex():
+    global canvas
+    canvas.delete("lascircle")
 
 def main():
 
@@ -48,20 +66,29 @@ def main():
     width,height = img.size
     root = Tk()
     root.title("frontal view")
-    root.geometry(str(width) + "x" + str(height))
+    root.geometry(str(width) + "x" + str(height+120))
     root.configure(background='grey')
 
-    canvas = Canvas(root,bg= 'grey')
+    canvas = Canvas(root,bg= 'white')
     canvas.pack(anchor = 'nw', fill = 'both', expand = 1)
-
-    update_panel = PanedWindow(orient ='horizontal')
-
-    
-    canvas.bind("<Button-1>", get_x_and_y)
-    # canvas.bind("<B1-Motion>", draw_smth)
 
     tkimg = ImageTk.PhotoImage(img)
     canvas.create_image(0,0,image = tkimg, anchor = 'nw')
+
+    add_button = Button(canvas, text = "add", command = add_vertex)
+    add_button.place(relx = .025, y = height+20, relwidth = 0.3, height = 80)
+
+    update_button = Button(canvas, text = "update")
+    update_button.place(relx = .35, y = height+20, relwidth = 0.3, height = 80)
+
+    cancel_button = Button(canvas, text = "cancel", command = cancel_vertex)
+    cancel_button.place(relx = .675, y = height+20, relwidth = 0.3, height = 80)
+
+    
+    canvas.bind("<Button-1>", select_point)
+    # canvas.bind("<B1-Motion>", draw_smth)
+
+    
 
     # ttk.Label(root, image = tkimg).place(x=0,y=0)
 
