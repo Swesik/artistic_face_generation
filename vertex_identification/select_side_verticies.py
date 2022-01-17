47739cbb2499c823ad0a4d0087c83bfc4d53f32a
SRTJHSST <swesik@gmail.com>
2022-01-17 16:02:18 -0500

    select and store verticies on image

--- vertex_identification/select_side_verticies.py
+++ vertex_identification/select_side_verticies.py
@@ -17,28 +17,27 @@ from PIL import ImageTk, Image
 # 	    side_features.append((x,y))
 
 def get_x_and_y(event):
-    global lasx, lasy, core_features
-    update_type = input("add, update, or cancel? [a,u,c]")
+    global canvas, core_features
+    lasx, lasy = event.x, event.y
+    update_type = input("add, update, or cancel [a,u,c]? -----")
     if update_type == 'a':
-        pass
+        core_features.append((lasx,lasy))
+        canvas.create_oval(lasx-5, lasy-5, lasx+5, lasy+5,fill="green", tags="circle"+str(len(core_features)))
+        canvas.create_text(lasx, lasy, text=str(len(core_features)), fill="black", font=('Helvetica 15 bold'), tags="circle"+str(len(core_features)))
     elif update_type == 'u':
-        to_update = input("which number do you want to update")
-        pass
+        to_update = input("which number do you want to update? ----- ")
+        core_features[int(to_update)] = core_features.append((lasx,lasy))
+        canvas.delete("circle"+to_update)
+        canvas.create_oval(lasx-6, lasy-6, lasx+6, lasy+6,fill="green", tags="circle"+to_update)
+        canvas.create_text(lasx, lasy, text=to_update, fill="black", font=('Helvetica 13 bold'), tags="circle"+to_update)
     elif update_type == 'c':
         pass
     else:
         print('error, wrong input')
         return
-    lasx, lasy = event.x, event.y
-
+    
+    
     print("(" + str(lasx) + "," + str(lasy) + ")")
-
-def draw_smth(event):
-    global lasx, lasy
-    canvas.create_line((lasx, lasy, event.x, event.y), 
-                      fill='red', 
-                      width=2)
-    lasx, lasy = event.x, event.y
 
 def main():
 
@@ -55,9 +54,11 @@ def main():
     canvas = Canvas(root,bg= 'grey')
     canvas.pack(anchor = 'nw', fill = 'both', expand = 1)
 
+    update_panel = PanedWindow(orient ='horizontal')
+
     
     canvas.bind("<Button-1>", get_x_and_y)
-    canvas.bind("<B1-Motion>", draw_smth)
+    # canvas.bind("<B1-Motion>", draw_smth)
 
     tkimg = ImageTk.PhotoImage(img)
     canvas.create_image(0,0,image = tkimg, anchor = 'nw')
