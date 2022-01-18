6a3a370e98f3ebb40a9c8c59cc5ae6c43682155a
SRTJHSST <swesik@gmail.com>
2022-01-18 12:49:47 -0500

    identify points on both images

--- vertex_identification/select_side_verticies.py
+++ vertex_identification/select_side_verticies.py
@@ -2,87 +2,110 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
 
-# import cv2
-# import numpy
-# import math
-
-# side_features = []
-# def select_point(event, x, y, flags, param):
-# 	# grab references to the global variables
-# 	global side_features
-# 	# if the left mouse button was clicked, record the starting
-# 	# (x, y) coordinates and indicate that cropping is being
-# 	# performed
-# 	if event == cv2.EVENT_LBUTTONDOWN:
-# 	    side_features.append((x,y))
-
-# def get_x_and_y(event):
-#     global canvas, core_features
-#     lasx, lasy = event.x, event.y
-#     update_type = input("add, update, or cancel [a,u,c]? -----")
-#     if update_type == 'a':
-#         core_features.append((lasx,lasy))
-#         canvas.create_oval(lasx-5, lasy-5, lasx+5, lasy+5,fill="green", tags="circle"+str(len(core_features)))
-#         canvas.create_text(lasx, lasy, text=str(len(core_features)), fill="black", font=('Helvetica 15 bold'), tags="circle"+str(len(core_features)))
-#     elif update_type == 'u':
-#         to_update = input("which number do you want to update? ----- ")
-#         core_features[int(to_update)] = core_features.append((lasx,lasy))
-#         canvas.delete("circle"+to_update)
-#         canvas.create_oval(lasx-6, lasy-6, lasx+6, lasy+6,fill="green", tags="circle"+to_update)
-#         canvas.create_text(lasx, lasy, text=to_update, fill="black", font=('Helvetica 13 bold'), tags="circle"+to_update)
-#     elif update_type == 'c':
-#         pass
-#     else:
-#         print('error, wrong input')
-#         return
-
 def select_point(event):
-    global canvas, lasx, lasy
-    lasx, lasy = event.x, event.y
-    canvas.delete("lascircle")
-    canvas.create_oval(lasx-5, lasy-5, lasx+5, lasy+5,fill="cyan", tags="lascircle")
-    
-    
-    print("(" + str(lasx) + "," + str(lasy) + ")")
+    global lasx_front, lasy_front, lasx_side, lasy_side
+    if event.y > max(height1,height2):
+        pass
+    elif event.x < width1:
+        lasx_front, lasy_front = event.x, event.y
+        canvas.delete("las_front_circle")
+        canvas.create_oval(lasx_front-5, lasy_front-5, lasx_front+5, lasy_front+5,fill="cyan", tags="las_front_circle")
+    elif event.x < width1 + 10:
+        pass
+    else:
+        lasx_side, lasy_side = event.x, event.y
+        canvas.delete("las_side_circle")
+        canvas.create_oval(lasx_side-5, lasy_side-5, lasx_side+5, lasy_side+5,fill="cyan", tags="las_side_circle")
+
 
 def add_vertex():
-    global canvas, core_features
-    canvas.delete("lascircle")
-    r = 6
-    canvas.create_oval(lasx-r, lasy-r, lasx+r, lasy+r,fill="green", tags="circle"+str(len(core_features)))
-    canvas.create_text(lasx, lasy, text=str(len(core_features)), fill="white", font=('Helvetica 12 bold'), tags="circle"+str(len(core_features)))
-    core_features.append((lasx,lasy))
+    # global canvas, core_features
+    # print(canvas.find_withtag("las_front_circle"))
+    try:
+        lasx_front
+    except NameError:
+        print('no points selected')
+        return
+    if len(canvas.find_withtag("las_front_circle")) < 1:
+        print('no frontal point selected')
+        return
+    if len(canvas.find_withtag("las_side_circle")) < 1:
+        print('no frontal point selected')
+        return
 
-def cancel_vertex():
-    global canvas
+    # print(canvas.find_withtag("las_front_circle"))
+
+    canvas.delete("las_front_circle")
+    canvas.delete("las_side_circle")
+    r = 5
+    canvas.create_oval(lasx_front-r, lasy_front-r, lasx_front+r, lasy_front+r,fill="green", tags="circle"+str(len(core_features)))
+    canvas.create_text(lasx_front, lasy_front, text=str(len(core_features)), fill="white", font=('Helvetica 10'), tags="circle"+str(len(core_features)))
+
+    canvas.create_oval(lasx_side-r, lasy_side-r, lasx_side+r, lasy_side+r,fill="green", tags="circle"+str(len(core_features)))
+    canvas.create_text(lasx_side, lasy_side, text=str(len(core_features)), fill="white", font=('Helvetica 10'), tags="circle"+str(len(core_features)))
+    
+    front_point = ((lasx_front/width1)*front_width,(lasy_front/height1)*front_height)
+    side_point = (((lasx_side-10-width2)/width2)*side_width,(lasy_front/height2)*side_height)
+
+    core_features.append((front_point,side_point))
+    
+    # lasx_front, lasy_front, lasx_side, lasy_side = -1, -1, -1, -1
+    # del lasx_front, lasy_front, lasx_side, lasy_side
+    # core_features.append((lasx,lasy))
+
+def save_vertecies():
+    # global canvas
     canvas.delete("lascircle")
 
 def main():
 
-    global root, canvas, core_features
+    global root, canvas, core_features, front_width, front_height, \
+        side_width, side_height, width1, height1, width2, height2
     core_features = []
-    path = "vertex_identification/Riely_front.png"
-    img = Image.open(path)
-    width,height = img.size
+    #read images
+    frontal_image_path = "vertex_identification/vincent_front.jpg"
+    frontal_img = Image.open(frontal_image_path)
+    front_width, front_height = frontal_img.size
+    width1 = 500
+    height1 = int(width1 * (front_height/front_width))
+    if height1 > 700:
+        height1 = 700
+        width1 = int(height1 * (front_width/front_height))
+    frontal_img = frontal_img.resize((width1,height1))
+
+    side_image_path = "vertex_identification/vincent_side.jpg"
+    side_img = Image.open(side_image_path)
+    side_width, side_height = side_img.size
+    height2 = height1
+    width2 = int(height1 * (side_width/side_height))
+    side_img = side_img.resize((width2,height2))
+
+    #create tkinter window
     root = Tk()
     root.title("frontal view")
-    root.geometry(str(width) + "x" + str(height+120))
+    root.geometry(str(width1+width2+10) + "x" + str(max(height1,height2)+120))
     root.configure(background='grey')
 
+    #create canvas
     canvas = Canvas(root,bg= 'white')
     canvas.pack(anchor = 'nw', fill = 'both', expand = 1)
 
-    tkimg = ImageTk.PhotoImage(img)
-    canvas.create_image(0,0,image = tkimg, anchor = 'nw')
+    #add images to canvas
+    tkimg_front = ImageTk.PhotoImage(frontal_img)
+    canvas.create_image(0,0,image = tkimg_front, anchor = 'nw')
 
+    tkimg_side = ImageTk.PhotoImage(side_img)
+    canvas.create_image(width1+10,0,image = tkimg_side, anchor = 'nw')
+
+    #add buttons to canvas
     add_button = Button(canvas, text = "add", command = add_vertex)
-    add_button.place(relx = .025, y = height+20, relwidth = 0.3, height = 80)
+    add_button.place(relx = .025, y = height1+20, relwidth = 0.3, height = 80)
 
-    update_button = Button(canvas, text = "update")
-    update_button.place(relx = .35, y = height+20, relwidth = 0.3, height = 80)
+    # update_button = Button(canvas, text = "update")
+    # update_button.place(relx = .35, y = height1+20, relwidth = 0.3, height = 80)
 
-    cancel_button = Button(canvas, text = "cancel", command = cancel_vertex)
-    cancel_button.place(relx = .675, y = height+20, relwidth = 0.3, height = 80)
+    # cancel_button = Button(canvas, text = "cancel", command = cancel_vertex)
+    # cancel_button.place(relx = .675, y = height1+20, relwidth = 0.3, height = 80)
 
     
     canvas.bind("<Button-1>", select_point)
